aa802430ec11b7f869fbc6499e27bf1b08b1c1ac
John Spurgin <Atchett@users.noreply.github.com>
2018-09-13 20:48:42 +0100

    initial commit before any compilation

---
+++ script/script.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+(function () {
+
+    // Nav toggle
+    var navChk = document.getElementById("menuChk"),
+        navUl = document.getElementsByClassName("menu");
+
+    navChk.onchange = function () {
+        navUl[0].classList.toggle("hide");
+    };
+
+    // footer date
+    var d = new Date(),
+        y = d.getFullYear(),
+        cDate = document.getElementById("cDate");
+
+    cDate.innerHTML += y;
+})();
+
