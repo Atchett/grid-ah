c50ef286b84534527b2f4bbed575e95ec4ec53b3
John Spurgin <Atchett@users.noreply.github.com>
2018-09-18 19:42:39 +0100

    Removed folders that are not required

--- dist/script/script.js
+++
@@ -1,1 +0,0 @@
-"use strict";!function(){var e=document.getElementById("menuChk"),t=document.getElementsByClassName("menu");e.onchange=function(){t[0].classList.toggle("hide")};var n=new Date,a=n.getFullYear();document.getElementById("cDate").innerHTML+=a}();
